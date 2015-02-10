adc2e13b4f70adf893c78d8c53db0115471264b3
akashsri99.Mac <zarvis-X@Akashs-MacBook-Pro.local>
2015-02-11 02:45:06 +0530

    hgfhg

--- django/urpoll/poll/admin.py
+++ django/urpoll/poll/admin.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
-from poll.models import category,Question,option_2,option_4,votes_ques
+from poll.models import category,Question,votes_ques
 # Register your models here.
-class option2Inline(admin.TabularInline):
-	model=option_2
-
-class option4Inline(admin.TabularInline):
-	model=option_4
-
-class QuesAdmin(admin.ModelAdmin):
-	fieldsets=[]
-	inlines=[option2Inline,option4Inline]
-
 admin.site.register(category)
-admin.site.register(Question,QuesAdmin)
+admin.site.register(Question)
