8ae77f73c51322d357b7b2b3582935fbed722339
akashsri99.Mac <zarvis-X@Akashs-MacBook-Pro.local>
2015-02-11 20:16:50 +0530

    editing url file

--- django/urpoll/poll/urls.py
+++ django/urpoll/poll/urls.py
@@ -5,4 +5,5 @@ from poll import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
 		       url(r'^(?P<id>\d+)/$',views.cat,name='cat'),
+		       url(r'^(?P<id>\d+)/detail/$',views.detail,name='detail'),
 			)
