3808295f5e6bbc01c4e8a1e2d7fcdfc50958a82e
akashsri99.Mac <zarvis-X@Akashs-MacBook-Pro.local>
2015-02-12 02:35:25 +0530

    9

--- django/urpoll/poll/views.py
+++ django/urpoll/poll/views.py
@@ -21,11 +21,11 @@ def submit_form(request,id):
 	if request.POST['choice1']==1:
 		details.vote_1=details.vote_1+1
 
-	if request.POST["choice2"]==1:
+	if request.POST['choice2']==1:
 		details.vote_2=details.vote_2+1
-	if request.POST["choice3"]==1:
+	if request.POST['choice3']==1:
 		details.vote_3=details.vote_3+1
-	if request.POST["choice4"]==1:
+	if request.POST['choice4']==1:
 		details.vote_4=details.vote_4+1
 
 	return render(request,'urpoll/details.html',{'details':details})
