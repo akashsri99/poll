a618e23a48edc3c9244724dd92a9ad1aef356257
akashsri99.Mac <zarvis-X@Akashs-MacBook-Pro.local>
2015-02-12 01:07:12 +0530

    submit_form

--- django/urpoll/poll/views.py
+++ django/urpoll/poll/views.py
@@ -15,3 +15,6 @@ def cat(request,id):
 def detail(request,id):
 	details=Question.objects.get(pk=id)
 	return render(request,'urpoll/details.html',{'details':details})
+
+def submit_form(request):
+	return render(request,'urpoll/details.html')
