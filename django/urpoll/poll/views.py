dfa91c997471f7f3e31124c4280b55bcfadd7856
akashsri99.Mac <zarvis-X@Akashs-MacBook-Pro.local>
2015-02-12 02:02:37 +0530

    3

--- django/urpoll/poll/views.py
+++ django/urpoll/poll/views.py
@@ -17,7 +17,7 @@ def detail(request,id):
 	return render(request,'urpoll/details.html',{'details':details})
 
 def submit_form(request,id):
-	details = get_object_or_404(Question, pk=question_id)
+	details = get_object_or_404(Question, pk=id)
 	'''details=request.POST['choice']
 '''
 	return render(request,'urpoll/details.html',{'details':details})
