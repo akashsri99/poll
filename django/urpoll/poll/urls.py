52e8455b7780bab6c6ac3c0f5f0706deea7ccd2d
akashsri99.Mac <zarvis-X@Akashs-MacBook-Pro.local>
2015-02-12 01:10:07 +0530

    updating urls

--- django/urpoll/poll/urls.py
+++ django/urpoll/poll/urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
 		       url(r'^(?P<id>\d+)/$',views.cat,name='cat'),
 		       url(r'^(?P<id>\d+)/detail/$',views.detail,name='detail'),
+
+		       url(r'^submit/$',views.submit_form,name='submit_form'),
 			)
