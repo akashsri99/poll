9d2db46336f4de7b75924cd6a2af4ca210fb97c1
akashsri99 <akashsri99@gmail.com>
2015-02-14 23:53:07 +0530

    Revert 745ab31..744f66d
    This rolls back to commit 745ab3114446469928866bf0d9fd15f635840ab6.

--- django/urpoll/poll/views.py
+++ django/urpoll/poll/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from django.template import RequestContext,loader
-from poll.models import category,Question,comment_A
+from poll.models import category,Question
 
 def index(request):
 	categorys=category.objects.order_by('Name')
@@ -22,11 +22,6 @@ def submit_form(request,id):
 	details = get_object_or_404(Question, pk=id)
 	if request.POST.get('choice')=="1":
 		details.vote_1=details.vote_1+1
-		if request.POST.get('reason'):
-			text1=request.POST.get('reason')
-			comment=comment_A(Question=id,userid=1,text=text1)
-			comment.save()
-
 		
 
 	if request.POST.get('choice')=="2":
