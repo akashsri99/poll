889786a01ce3ab35d8d035453113c4ffec49a852
akashsri99 <akashsri99@gmail.com>
2015-02-15 13:53:49 +0530

    Redirecting URL

--- django/urpoll/urpoll/urls.py
+++ django/urpoll/urpoll/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'urpoll.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$',RedirectView.as_view(url='http://upollet.com/home'),name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^home/',include('poll.urls')),
