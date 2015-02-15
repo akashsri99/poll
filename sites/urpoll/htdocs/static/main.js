7083946dde66b919a3a4d5b2d8ec3f99e9e82f2f
akashsri99 <akashsri99@gmail.com>
2015-02-15 23:26:27 +0530

    20

--- sites/urpoll/htdocs/static/main.js
+++ sites/urpoll/htdocs/static/main.js
@@ -1,3 +1,7 @@
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script> 
+<script src="http://malsup.github.com/jquery.form.js"></script> 
 <script>  
         $(document).ready(function() {
 
