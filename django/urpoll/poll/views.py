b005b0dee70b90b61f4718ae69480441322cbe82
akashsri99 <akashsri99@gmail.com>
2015-02-21 01:29:40 +0530

    3

--- django/urpoll/poll/views.py
+++ django/urpoll/poll/views.py
@@ -80,7 +80,7 @@ def get_comments_D(request,id):
 
 def submit_question(request,id):
 	c=category.objects.filter(Name=request.POST.get('Category'))
-	q=Question(category=c.id,question=request.POST.get('question'),option1=request.POST.get('opA'),option2=request.POST.get('opB'),option3=request.POST.get('opC'),option4=request.POST.get('opD'))
+	q=Question(category=c,question=request.POST.get('question'),option1=request.POST.get('opA'),option2=request.POST.get('opB'),option3=request.POST.get('opC'),option4=request.POST.get('opD'))
 	q.save()
 	categorys=category.objects.order_by('Name')
 	ques=Question.objects.order_by('-date')
