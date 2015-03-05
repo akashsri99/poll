a71813a05f25bfaead91a8b1d6ea901f74d7f6e9
akashsri99 <akashsri99@gmail.com>
2015-03-06 00:55:01 +0530

    js file

--- sites/urpoll/htdocs/static/app.js
+++ sites/urpoll/htdocs/static/app.js
@@ -1,4 +1,4 @@
-<script>
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -62,6 +62,6 @@
         'Thanks for logging in, ' + response.name + response.id +'!';
     });
   }
-</script>
+
   <!-- fb script ends  -->
     
