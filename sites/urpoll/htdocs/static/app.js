0783a3aa470e4036a64b5adf95723dd80ad5a2c3
akashsri99 <akashsri99@gmail.com>
2015-03-06 23:52:11 +0530

    next try

--- sites/urpoll/htdocs/static/app.js
+++ sites/urpoll/htdocs/static/app.js
@@ -24,7 +24,7 @@
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      $(input).hide();
+      $('input').hide();
       document.getElementById('status').innerHTML = 'Please log ' +
         'into Facebook.';
     }
