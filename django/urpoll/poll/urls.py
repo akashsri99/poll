ceabb150c0ea6546e9db7fa4ed5eb829c20e1c62
akashsri99 <akashsri99@gmail.com>
2015-04-07 22:44:54 +0530

    another test

--- django/urpoll/poll/urls.py
+++ django/urpoll/poll/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
 		       url(r'^(?P<id>\d+)/$',views.cat,name='cat'),
 		       url(r'^(?P<id>\d+)/detail/$',views.detail,name='detail'),
-		       url(r'^/submit_form/$',views.submit_form,name='submit_form'),
+		       url(r'^submit_form/$',views.submit_form,name='submit_form'),
 		       
 
 		       url(r'^(?P<id>\d+)/submit_form/$',views.submit_form,name='submit_form'),
