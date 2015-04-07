19e27e78fce3cfa1d543a5d3eca41bd00d84f67c
akashsri99 <akashsri99@gmail.com>
2015-04-07 22:49:58 +0530

    submit_question

--- django/urpoll/poll/urls.py
+++ django/urpoll/poll/urls.py
@@ -6,10 +6,10 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
 		       url(r'^(?P<id>\d+)/$',views.cat,name='cat'),
 		       url(r'^(?P<id>\d+)/detail/$',views.detail,name='detail'),
-		       url(r'^submit_form/$',views.submit_form,name='submit_form'),
+		       url(r'^submit_question/$',views.submit_question,name='submit_question'),
 		       
 
-		       url(r'^(?P<id>\d+)/submit_form/$',views.submit_form,name='submit_form'),
+		       url(r'^(?P<id>\d+)/submit_question/$',views.submit_question,name='submit_question'),
 		       url(r'^(?P<id>\d+)/reason_opt/$',views.reason_opt,name='reason_opt'),
 		       url(r'^(?P<id>\d+)/get_comments_A/$',views.get_comments_A,name='get_comments_A'),
 		       url(r'^(?P<id>\d+)/get_comments_B/$',views.get_comments_B,name='get_comments_B'),
