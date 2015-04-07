07b635afcf2832c9a2b0c12fb56496740aa5aec9
akashsri99 <akashsri99@gmail.com>
2015-04-07 22:40:45 +0530

    submit form

--- django/urpoll/poll/urls.py
+++ django/urpoll/poll/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
 		       url(r'^(?P<id>\d+)/$',views.cat,name='cat'),
 		       url(r'^(?P<id>\d+)/detail/$',views.detail,name='detail'),
+		       url(r'^/submit_form/$',views.submit_form,name='submit_form'),
+		       
 
 		       url(r'^(?P<id>\d+)/submit_form/$',views.submit_form,name='submit_form'),
 		       url(r'^(?P<id>\d+)/reason_opt/$',views.reason_opt,name='reason_opt'),
